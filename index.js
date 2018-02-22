b242ead1300866d37926d81a535be0453c95dd55
Aditya Mhamunkar <me@adityamhamunkar.com>
2018-02-22 01:31:01 -0500

    chore: Configured the templates structure using ejs
    - imported ejs
    - structured the application to support templating

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const mongoose = require('mongoose');
 const app = express();
 const api_route = require('./routes/api_route.js');
 
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -23,6 +28,12 @@ app.get('/hello', function(req, res){
 	res.send('<h1> Hello there, I am working! </h1>');
 });
 
+app.get('/', function(req, res){
+	var title = "Bappa App"
+	res.render('index', {
+		title: title
+	});
+});
 
 
 
