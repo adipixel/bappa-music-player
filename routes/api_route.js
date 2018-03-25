7ee501432025d2f01f99b37e3e15200bd18744d4
Aditya Mhamunkar <me@adityamhamunkar.com>
2018-03-25 02:02:22 -0400

    chore: created api structure to send song data

--- routes/api_route.js
+++ routes/api_route.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const router = express.Router();
+var fs = require('fs');
+
 
 const app = express();
 
-// add user
-router.post('/add', (req, res) => {
-	res.json({ hey: "hi"});
+
+var aarti_list = [
+	{"id": 1, "title": "Sukhakarta Dukhaharta", "text": "../resources/sukhakarta.txt"},
+	{"id": 2, "title": "Durge Durghat"}
+]
+
+var aarti_hashMap = new Map();
+aarti_hashMap.set(1, {"title": "Sukhakarta Dukhaharta", "text": "./resources/sukhakarta.txt"});
+aarti_hashMap.set(2, {"title": "Durge Durghat", "text": "./resources/sukhakarta.txt"});
+
+
+// list of aartis
+router.get('/aarti/list', (req, res) => {
+	res.json(aarti_list);
 });
+
+// aarti
+router.get('/aarti/:id', (req, res) => {
+	//res.json(aarti_hashMap.get(parseInt(req.params.id)));
+	var song = ""
+	fs.readFile(aarti_hashMap.get(parseInt(req.params.id)).text, 'utf8', function(err, data) {
+	    if (err) throw err;
+	    song = data;
+
+	    res.json({
+			title: aarti_hashMap.get(parseInt(req.params.id)).title,
+			song: song
+		});
+	});
+
+});
+
+
+
 
 module.exports = router;
